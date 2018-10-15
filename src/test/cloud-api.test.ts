17e58dcdc34f6db9a70e128629c66aa5fa8d0ae4
tom <tom_p@experitest.com>
2018-10-15 16:13:38 +0300

    CHANGE : Application URL

--- src/test/cloud-api.test.ts
+++ src/test/cloud-api.test.ts
@@ -2,19 +2,19 @@ import * as chai from 'chai';
 import * as chai_promise from 'chai-as-promised';
 import {CloudAPI} from '../lib/cloud-api';
 import {Device} from "../lib/device-interaction";
-import {DeviceModel} from "../modules/device";
+import {DeviceModel, DeviceOS} from "../modules/device";
 import {ApplicationModel} from "../modules/application";
 import {ACCESS_KEY, CLOUD_URL} from "./cloud-info";
+
 chai.use(chai_promise);
 
 // tslint:disable-next-line:max-line-length
 
 describe('cloud-api tests', function () {
 
-    before(() =>
-    {
+    before(() => {
         const assert = chai.assert;
-        assert.isNotEmpty(ACCESS_KEY,'ACCESS KEY SHOULD NOT BE EMPTY - PLEASE EDIT ./src/test/cloud-info.ts');
+        assert.isNotEmpty(ACCESS_KEY, 'ACCESS KEY SHOULD NOT BE EMPTY - PLEASE EDIT ./src/test/cloud-info.ts');
         assert.isNotEmpty(CLOUD_URL, 'CLOUD DOMAIN SHOULD NOT BE EMPTY -  PLEASE EDIT ./src/test/cloud-info.ts');
     })
     it('#fetchDevices', async function () {
@@ -22,14 +22,6 @@ describe('cloud-api tests', function () {
         const cloud = new CloudAPI(CLOUD_URL, ACCESS_KEY);
         const devices: DeviceModel [] = await cloud.fetchDevices();
         console.table(devices);
-    });
-
-    it('#open Web Socket', async function () {
-        this.timeout(120 * 1000);
-        const cloud = new CloudAPI(CLOUD_URL, ACCESS_KEY);
-        const webSocketInfo = await (await cloud.getWebSocketInfo());
-        const device = new Device(webSocketInfo.deviceId, cloud, webSocketInfo.externalLink);
-        device.addScreenListner((screenshot) => console.log(screenshot));
     });
 
     it("# fetches applications", async function () {
@@ -42,7 +34,24 @@ describe('cloud-api tests', function () {
     it('#uploads applications to the cloud', async function () {
         const cloud = new CloudAPI(CLOUD_URL, ACCESS_KEY);
         this.timeout(120 * 1000);
-        const applicationId = await cloud.uploadApplication("http://d242m5chux1g9j.cloudfront.net/eribank.apk");
+        const applicationId = await cloud.uploadApplication("https://s3-us-west-2.amazonaws.com/seetest-io-demo-apps/EriBank.ipa");
         console.log(applicationId);
+    });
+
+    it('#open Web Socket', async function () {
+        this.timeout(120 * 1000);
+        const cloud = new CloudAPI(CLOUD_URL, ACCESS_KEY);
+        const deviceInfo = await cloud.findAvailableDevice(DeviceOS.Android);
+        if (deviceInfo === undefined) {
+            console.warn("SKIPPED TEST - NO AVAILABLE DEVICE")
+        }
+        else {
+            setTimeout(async () => {
+                const webSocketInfo = await (await cloud.getWebSocketInfo(deviceInfo));
+                const device = new Device(webSocketInfo.deviceId, cloud, webSocketInfo.externalLink);
+                await device.addScreenListner((screenshot) => console.log(screenshot));
+            },10*1000);
+
+        }
     });
 });
