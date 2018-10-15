0ec90122f12018b39cd4030323fa791a19ef0eef
tom <tom_p@experitest.com>
2018-10-15 11:58:53 +0300

    INIT : cloud-info file

---
+++ src/test/cloud-info.ts
@@ -0,0 +1,4 @@
+// ACCESS_KEY How to obtain access key - https://docs.seetest.io/display/SEET/Execute+Tests+on+SeeTest+-+Obtaining+Access+Key
+// CLOUD_URL - PLACE YOUR CLOUD URL HERE - e.g https://cloud.seetest.io
+export const ACCESS_KEY = "";
+export const CLOUD_URL = "";
